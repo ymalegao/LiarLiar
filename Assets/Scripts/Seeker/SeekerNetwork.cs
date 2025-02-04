ff025288c1da21bc06f4d5720b11b5e757cc27d0
zokamou <122842610+zokamou@users.noreply.github.com>
2025-02-04 09:48:29 -0800

    cleanup

--- Assets/Scripts/Seeker/SeekerNetwork.cs
+++ Assets/Scripts/Seeker/SeekerNetwork.cs
@@ -47,30 +47,6 @@ public class PlayerNetwork : NetworkBehaviour
 
     }
 
-    // Update is called once per frame
-    //     void Update()
-    // {
-    //         if (Input.GetKeyDown(KeyCode.Space))
-    //         {
-    //             randomNumber.Value = Random.Range(1, 100);
-    //         }
-
-    //         // Allow movement if the player is the owner or if the network is not running
-    //         if (NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsServer)
-    //     {
-    //         if (!IsOwner) return; // Only process movement for the owning player
-    //     }
-
-    //     animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-    //     Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);;
-    //     transform.position = transform.position + horizontal * Time.deltaTime * 5;
-
-    //     animator.SetFloat("vertical", Input.GetAxis("Vertical"));
-    //     Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"),  0.0f);;
-    //     transform.position = transform.position + vertical * Time.deltaTime * 5;
-
-    // }
-
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             randomNumber.Value = Random.Range(1, 100);
