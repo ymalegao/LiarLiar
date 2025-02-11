f0e81434fd8081b8bb61e855515267329794df2a
firekicks <114191519+firekicks@users.noreply.github.com>
2025-02-10 23:47:39 -0800

    pushing minigame

---
+++ Assets/Scripts/WoodChopMinigame/Axe.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wood"))
+        {
+            WoodGameManager.Instance.AddScore();
+            Destroy(other.gameObject);
+        }
+    }
+}
