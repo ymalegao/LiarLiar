59e8cc79c605feb0e563d67b853bdf17cce740c9
zokamou <122842610+zokamou@users.noreply.github.com>
2025-02-12 12:53:05 -0800

    NPC spawnpoints

--- Assets/Scripts/NPC/NpcSpawner.cs
+++ Assets/Scripts/NPC/NpcSpawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class NPCSpawner : NetworkBehaviour
 {
   [SerializeField] private GameObject[] npcPrefabs; // Array of NPC prefabs
-  [SerializeField] private Transform[] waypoints;
   [SerializeField] private Vector3[] spawnPositions; // Array of spawn positions
 
   public override void OnNetworkSpawn()
@@ -29,56 +28,7 @@ public class NPCSpawner : NetworkBehaviour
       int prefabIndex = i % npcPrefabs.Length; // Ensure index is within range
       GameObject npcInstance = Instantiate(npcPrefabs[prefabIndex], spawnPositions[i], Quaternion.identity);
 
-      var npcMovement = npcInstance.GetComponent<NpcMovement>();
-      if (npcMovement != null)
-      {
-        npcMovement.SetWaypoints(waypoints);
-      }
-      else
-      {
-        Debug.LogError($"NpcMovement component is missing from {npcPrefabs[prefabIndex].name}");
-      }
-
       npcInstance.GetComponent<NetworkObject>().Spawn();
     }
   }
 }
-// using Unity.Netcode;
-// using UnityEngine;
-
-// public class NPCSpawner : NetworkBehaviour
-// {
-//     [SerializeField] private GameObject npcPrefab;
-//     [SerializeField] private Transform[] waypoints;
-//     [SerializeField] private Vector3 spawnPosition;
-
-//     public override void OnNetworkSpawn()
-//     {
-//         if (IsServer)
-//         {
-//             SpawnNPC();
-//         }
-//     }
-
-//     private void SpawnNPC()
-//     {
-//         if (npcPrefab == null)
-//         {
-//             Debug.LogError("NPC prefab is null");
-//             return;
-//         }
-
-//         GameObject npcInstance = Instantiate(npcPrefab, spawnPosition, Quaternion.identity);
-
-//         var npcMovement = npcInstance.GetComponent<NpcMovement>();
-//         if (npcMovement != null)
-//         {
-//             npcMovement.SetWaypoints(waypoints);
-//         }
-//         else
-//         {
-//             Debug.LogError("NpcMovement component is missing from the NPC prefab");
-//         }
-//         npcInstance.GetComponent<NetworkObject>().Spawn();
-//     }
-// }
