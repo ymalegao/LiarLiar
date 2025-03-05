5ee41d054d4c3d89ca0c13eaaf8522a8d72c7007
AcerCristea <55897923+AcerCristea@users.noreply.github.com>
2025-03-04 20:23:27 -0800

    Host can send all to home screen.

--- Assets/GoToLobby.cs
+++ Assets/GoToLobby.cs
@@ -2,17 +2,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Unity.Netcode;
 
-public class goToScene : MonoBehaviour
+public class goToScene : NetworkBehaviour
 {
     public void goToLobby()
     {
-        if (NetworkManager.Singleton != null && (NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient))
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsServer)
         {
-            Debug.Log("Shutting down Netcode before returning to the Lobby...");
+            Debug.Log("Host is returning to the lobby...");
+
+            // Notify all clients to return to the lobby
+            LoadLobbyClientRpc();
+
+            // Host loads the lobby scene
+            SceneManager.LoadScene("Lobby");
+        }
+        else if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsClient)
+        {
+            Debug.Log("Client is waiting for host to return to the lobby...");
+        }
+        else
+        {
+            // If not connected, just load the lobby scene
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+
+    [ClientRpc]
+    private void LoadLobbyClientRpc()
+    {
+        Debug.Log("Client received instruction to return to the lobby...");
+
+        // Shutdown the network manager (if connected)
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsClient)
+        {
             NetworkManager.Singleton.Shutdown();
         }
 
-        SceneManager.LoadScene("Lobby"); 
+        // Load the lobby scene
+        SceneManager.LoadScene("Lobby");
     }
 
     public void goToTitle()
